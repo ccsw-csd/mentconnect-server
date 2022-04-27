9acf5c2f9e9d87e9780ce3d1ea7af4f2bd4a3782
Armen Mirzoyan Denisov <100298950+armirzoya@users.noreply.github.com>
2022-04-27 10:26:57 +0200

    initial commit

---
+++ src/main/java/com/capgemini/mentconnectserver/MentconnectServerApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.mentconnectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentconnectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentconnectServerApplication.class, args);
+	}
+
+}
