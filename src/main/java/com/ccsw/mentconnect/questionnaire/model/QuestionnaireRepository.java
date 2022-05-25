40d6bd7b0e020300a1936581c106dcec88b9ea8e
jogutierrez99 <joshuaalejandro1999@gmail.com>
2022-05-25 09:27:35 +0200

    test findPage sin filtro

--- src/main/java/com/ccsw/mentconnect/questionnaire/model/QuestionnaireRepository.java
+++ src/main/java/com/ccsw/mentconnect/questionnaire/model/QuestionnaireRepository.java
@@ -2,12 +2,13 @@ package com.ccsw.mentconnect.questionnaire.model;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface QuestionnaireRepository extends PagingAndSortingRepository<QuestionnaireEntity, Long> {
-	
-	//Preguntar
-	@Override
-	List<QuestionnaireEntity> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = { "user" })
+    List<QuestionnaireEntity> findAll();
 
 }
--- src/test/java/com/ccsw/mentconnect/questionnaireTest/QuestionnaireTest.java
+++ src/test/java/com/ccsw/mentconnect/questionnaireTest/QuestionnaireTest.java
@@ -13,7 +13,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
+import com.ccsw.mentconnect.questionnaire.dto.QuestionnaireSearchDto;
 import com.ccsw.mentconnect.questionnaire.logic.QuestionnaireServiceImpl;
 import com.ccsw.mentconnect.questionnaire.model.QuestionnaireEntity;
 import com.ccsw.mentconnect.questionnaire.model.QuestionnaireRepository;
@@ -25,10 +29,10 @@ public class QuestionnaireTest {
     private QuestionnaireRepository questionnaireRepository;
 
     @InjectMocks
-    private QuestionnaireServiceImpl questionnaireService;
+    private QuestionnaireServiceImpl questionnaireServiceImpl;
 
     @Test
-    public void findAllShouldReturnAllQuestionnaires() {
+    public void findAllShouldReturnAllQuestionnaire() {
 
         List<QuestionnaireEntity> list = new ArrayList<>();
 
@@ -36,10 +40,31 @@ public class QuestionnaireTest {
 
         when(questionnaireRepository.findAll()).thenReturn(list);
 
-        List<QuestionnaireEntity> questionnaires = questionnaireService.findAll();
+        List<QuestionnaireEntity> questionnaires = questionnaireServiceImpl.findAll();
 
         assertNotNull(questionnaires);
         assertEquals(1, questionnaires.size());
+    }
+
+    @Test
+    public void findPageShouldReturnPageQuestionnaire() {
+
+        List<QuestionnaireEntity> list = new ArrayList<>();
+
+        list.add(mock(QuestionnaireEntity.class));
+
+        QuestionnaireSearchDto dto = new QuestionnaireSearchDto();
+
+        dto.setPageable(PageRequest.of(0, 10));
+
+        when(questionnaireRepository.findAll(dto.getPageable()))
+                .thenReturn(new PageImpl<>(list, dto.getPageable(), list.size()));
+
+        Page<QuestionnaireEntity> questionnaire = questionnaireServiceImpl.findPage(dto);
+
+        assertNotNull(questionnaire);
+        assertEquals(1, questionnaire.getContent().size());
+
     }
 
 }
