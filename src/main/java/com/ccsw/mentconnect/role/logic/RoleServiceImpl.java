fea75bf916a15e4948f5af338e9dde5b68983d1f
Maribel3 <104841277+Maribel3@users.noreply.github.com>
2022-06-01 16:00:15 +0200

    Update RoleServiceImpl.java

--- src/main/java/com/ccsw/mentconnect/role/logic/RoleServiceImpl.java
+++ src/main/java/com/ccsw/mentconnect/role/logic/RoleServiceImpl.java
@@ -1,10 +1,7 @@
 package com.ccsw.mentconnect.role.logic;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ccsw.mentconnect.common.mapper.BeanMapper;
 import com.ccsw.mentconnect.role.model.RoleEntity;
 import com.ccsw.mentconnect.role.model.RoleRepository;
