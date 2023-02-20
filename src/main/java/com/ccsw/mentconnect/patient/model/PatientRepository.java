27a3c0fc4e0d4da0304e5b5e5c84cacf6820734d
Armen Mirzoyan Denisov <100298950+armirzoya@users.noreply.github.com>
2023-02-20 14:52:04 +0100

    Update PatientRepository.java

--- src/main/java/com/ccsw/mentconnect/patient/model/PatientRepository.java
+++ src/main/java/com/ccsw/mentconnect/patient/model/PatientRepository.java
@@ -1,14 +1,24 @@
 package com.ccsw.mentconnect.patient.model;
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 
 public interface PatientRepository extends PagingAndSortingRepository<PatientEntity, Long>, JpaSpecificationExecutor<PatientEntity> {
+
+    @Override
     @EntityGraph(attributePaths = {"user"})
     List<PatientEntity> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = {"user"})
+    Page<PatientEntity> findAll(Specification spec, Pageable pageable);
 
     boolean existsByNif(String nif);
 }
